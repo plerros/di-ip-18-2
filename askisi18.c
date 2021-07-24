358a7e3260df26988f8967d9e903bf89d0aaeb23
plerros <48536508+plerros@users.noreply.github.com>
2021-07-25 01:56:46 +0300

    code cleanup

--- askisi18.c
+++ askisi18.c
@@ -2,105 +2,97 @@
 //to show debug printf's gcc ... -DDEBUG
 //#define DEBUG
 //======================================================================================================================================================
-int operatorchk(int current)
+int is_operator(int c)
 {
-	return(current == '+' || current == '-' || current == '*');
+	return(c == '+' || c == '-' || c == '*');
 }
-int numberchk(int current)
+
+int is_number(int c)
 {
-	return(current >= '0' && current <= '9');
+	return(c >= '0' && c <= '9');
 }
-int signchk(int current)
+
+int is_sign(int c)
 {
-	return(current == '+' || current == '-');
+	return(c == '+' || c == '-');
 }
-int spacechk(int current)
+
+int is_space(int c)
 {
-	return(current == ' ' || current == '\t');
+	return(c == ' ' || c == '\t');
 }
-int endchk(int current)
+
+int is_end(int c)
 {
-	return(current == '\n'|| current == ')');
+	return(c == '\n' || c == ')');
+}
+
+int is_any(int c)
+{
+	return(is_operator(c) || is_number(c) || is_space(c) || is_end(c) || c == '(');
 }
 
 //======================================================================================================================================================
-int input(int *error) {
-	int in = getchar();
-	if (in == EOF)
+int read(int *error)
+{
+	int c = getchar();
+	if (c == EOF)
 		*error = 5;
-	else if (!operatorchk(in) && !numberchk(in) && !spacechk(in) && !endchk(in) && in != '(')
+	else if (!is_any(c))
 		*error = 1;
-	return(in);
+	return c;
 }
 //======================================================================================================================================================
-int handlerror(int *error, int *parenthesiscount) {
-	if (*error) {																	//error 0 == no error
-		if (*error == 1) {
+void handle_error(int *error, int *parenthesiscount) {
+	switch (*error)
+	{
+		case 0:	//error 0 == no error
+			if (*parenthesiscount > 0)
+				printf("error: Missing ')'.\n");	//error :more '(' than ')'
+			else if (*parenthesiscount < 0)
+				printf("error: Missing '('.\n");	//error :less '(' than ')'
+		case 1: //error 1 == invalid input
 			printf("error: Valid characters are: {+, -, *, 1..9, comma, space, tab, newline}.\n");
-		}					//error 1 == invalid input
-		else if (*error == 2) {
-			printf("error: One operator between a pair of numbers, or between a number and  parenthesis, is needed.\n");
-		}	//error 2 == missing operator
-		else if (*error == 3) {
-			printf("error: A number or parenthesis open is expected after an operator or a sign.\n");
-		}			//error 3 == missing number
-		else if (*error == 4) {
-			printf("error: Empty input ' ' or '( )' isn't a valid mathematical expression.\n");
-		}					//error 4 == empty calcline initialization
-		else if (*error == 5) {
+			break;
+		case 2:	//error 2 == missing operator
+			printf("error: One operator between a pair of numbers, or between a number and a parenthesis, is needed.\n");
+			break;
+		case 3:	//error 3 == missing number
+			printf("error: A number or parenthesis_open is expected after an operator or a sign.\n");
+			break;
+		case 4:	//error 4 == empty calcline initialization
+			printf("error: Empty input ' ' or '( )' isn't a valid expression.\n");
+			break;
+		case 5: //error 5 == no error just EOF
 			printf("\n");
-			return 0;
-		}													//error 5 == no error just EOF
-		else if (*error == 6) {
+			break;
+		case 6:	//error 6 == too many signs
 			printf("error: There can be at most one sign per number\n");
-		}							//error 6 == too many signs
-		return 0;
-	} else {
-		if (*parenthesiscount > 0) {
-			printf("error: Missing ')'.\n");
-		}									//error :more '(' than ')'
-		else if (*parenthesiscount < 0) {
-			printf("error: Missing '('.\n");
-		}									//error :less '(' than ')'
-		return 0;
+			break;
 	}
 }
 //======================================================================================================================================================
-int signadd(int current)
+int spaceskip(int *error)
 {
-	if (signchk(current))
-		return current;
-	else
-		return 0;
-}
-int signn(int sign, int number)
-{
-	if (sign)
-		return (number * (44 - sign));
-	else
-		return number;
-}
-
-//======================================================================================================================================================
-int spaceskip(int current, int *error)
-{
-	for (; spacechk(current); current = input(error)) {
+	int current;
+	do {
+		current = read(error);
 		if (*error)
 			return 0;
-	}
+	} while (is_space(current));
 	return current;
 }
 //======================================================================================================================================================
 int get_operator(int *error) {
-	int current = spaceskip(input(error), error);
+	int current = spaceskip(error);
 	if (*error)
 		return 0;
 
-	if (!operatorchk(current) && !endchk(current))
+	if (!is_operator(current) && !is_end(current))
 		*error = 2;
-	else if (endchk(current))
+	else if (is_end(current))
 		ungetc(current, stdin);
-	else if (operatorchk(current))
+	else if (is_operator(current))
 		return(current);
 
 	return 0;
@@ -121,35 +113,36 @@ int get_number(int current, int *error, int *parenthesiscount)
 #ifdef DEBUG
 													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf(")\n");
 #endif
-	}
-	else{
-		int sign = signadd(current);
-		if (sign) {
-			current = spaceskip(input(error), error);
-			if (*error) {
+	} else {
+		int sign = 1;
+		if (is_sign(current)) {
+			if (current == '-')
+				sign = -1;
+			current = spaceskip(error);
+			if (*error)
 				return 0;
-			}
-			else if (endchk(current)) {
+
+			if (is_end(current)) {
 				ungetc(current, stdin);
 				*error = 3;
 				return 0;
 			}
-			else if (signchk(current)) {
+			else if (is_sign(current)) {
 				*error = 6;
 				return 0;
 			}
 		}
-		else if (!numberchk(current)) {
+		else if (!is_number(current)) {
 			*error = 2;
 			return 0;
 		}
-		for (number = 0;numberchk(current);current = input(error)) {
+		for (number = 0;is_number(current);current = read(error)) {
 			if (*error)
 				return 0;
 			number *= 10;
 			number += current - '0';
 		}
-		number = signn(sign, number);
+		number *= sign;
 		ungetc(current, stdin);
 	}
 	return number;
@@ -157,19 +150,19 @@ int get_number(int current, int *error, int *parenthesiscount)
 //======================================================================================================================================================
 int calcline(int prevnumber, int operator, int *error, int *parenthesiscount)
 {
-	int number, operatornew, current = spaceskip(input(error), error);
+	int number, operatornew, current = spaceskip(error);
 	if (*error)
 		return 0;
 #ifdef DEBUG
 													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");} printf("-  -  -  -  -  -  -  -  -  \n");for (int i = 0;i<*parenthesiscount;i++) {printf("\t");} printf("initial prevnumber is: %d\noperator is: %c\n", prevnumber, operator);
 #endif
-	if (endchk(current)) {
+	if (is_end(current)) {
 		ungetc(current, stdin);
 		*error = 4;
 		return 0;
 	}
 
-	while (!endchk(current)) {
+	while (!is_end(current)) {
 		number = get_number(current, error, parenthesiscount);
 		if (*error)
 			return 0;
@@ -206,7 +199,7 @@ int calcline(int prevnumber, int operator, int *error, int *parenthesiscount)
 #ifdef DEBUG
 													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("-----------------------------\n");for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("prevnumber is: %d\nnewoperator is: %c\n", prevnumber, operator);
 #endif
-		current = spaceskip(input(error), error);
+		current = spaceskip(error);
 		if (*error)
 			return 0;
 	}
@@ -228,18 +221,16 @@ int main()
 		int parenthesiscount = 0;
 		int result = calcline(0, '+', &error, &parenthesiscount);
 		printf("Result %d: ", counter);
-		if (error || parenthesiscount) {
-			handlerror(&error, &parenthesiscount);
-		}
-		else{
+		if (error || parenthesiscount)
+			handle_error(&error, &parenthesiscount);
+		else
 			printf("%d\n", result);
-		}
 #ifdef DEBUG
 													printf("Remaining stdin:[%c", current);
 #endif
 		int current = 0;
 		while (current != '\n' && error != 5) {
-			current = input(&error);
+			current = read(&error);
 #ifdef DEBUG
 													printf("%c", current);
 #endif
