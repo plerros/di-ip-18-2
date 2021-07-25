7d0679df3a7206475be4871a7cee50d16b6641c0
plerros <48536508+plerros@users.noreply.github.com>
2021-07-25 15:27:38 +0300

    Code restructure & cleanup
    -Add debug_tabulate.
    -Add error 7 & 8 for parenthesis handling.
    -Split get_number() into read_unumber() and read_snumber().
    -Split calcline() into read_factor(), read_term() and read_expression().
    -EOF will no longer cause an empty result message.

--- askisi18.c
+++ askisi18.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 //to show debug printf's gcc ... -DDEBUG
-//#define DEBUG
+#define DEBUG
+//======================================================================================================================================================
+#ifdef DEBUG
+void debug_tabulate(int parenthesiscount)
+{
+	for (int i = 0; i < parenthesiscount; i++)
+		printf("\t");
+}
+
+#else /* DEBUG */
+void debug_tabulate(int parenthesiscount)
+{
+}
+
+#endif /* DEBUG */
+
 //======================================================================================================================================================
 int is_operator(int c)
 {
@@ -42,15 +57,13 @@ int read(int *error)
 		*error = 1;
 	return c;
 }
+
 //======================================================================================================================================================
 void handle_error(int *error, int *parenthesiscount) {
 	switch (*error)
 	{
 		case 0:	//error 0 == no error
-			if (*parenthesiscount > 0)
-				printf("error: Missing ')'.\n");	//error :more '(' than ')'
-			else if (*parenthesiscount < 0)
-				printf("error: Missing '('.\n");	//error :less '(' than ')'
+			break;
 		case 1: //error 1 == invalid input
 			printf("error: Valid characters are: {+, -, *, 1..9, comma, space, tab, newline}.\n");
 			break;
@@ -60,7 +73,7 @@ void handle_error(int *error, int *parenthesiscount) {
 		case 3:	//error 3 == missing number
 			printf("error: A number or parenthesis_open is expected after an operator or a sign.\n");
 			break;
-		case 4:	//error 4 == empty calcline initialization
+		case 4:	//error 4 == empty read_expression initialization
 			printf("error: Empty input ' ' or '( )' isn't a valid expression.\n");
 			break;
 		case 5: //error 5 == no error just EOF
@@ -69,8 +82,15 @@ void handle_error(int *error, int *parenthesiscount) {
 		case 6:	//error 6 == too many signs
 			printf("error: There can be at most one sign per number\n");
 			break;
+		case 7:	//error :more '(' than ')'
+			printf("error: Missing ')'.\n");
+			break;
+		case 8:	//error :less '(' than ')'
+			printf("error: Missing '('.\n");
+			break;
 	}
 }
+
 //======================================================================================================================================================
 int spaceskip(int *error)
 {
@@ -82,6 +102,7 @@ int spaceskip(int *error)
 	} while (is_space(current));
 	return current;
 }
+
 //======================================================================================================================================================
 int get_operator(int *error) {
 	int current = spaceskip(error);
@@ -93,68 +114,136 @@ int get_operator(int *error) {
 	else if (is_end(current))
 		ungetc(current, stdin);
 	else if (is_operator(current))
-		return(current);
+		return current;
 
 	return 0;
 }
+
 //======================================================================================================================================================
-int calcline(int prevnumber, int operator, int *error, int *parenthesiscount);
-int get_number(int current, int *error, int *parenthesiscount)
+int read_unumber(int *error)
 {
-	int number;
-	if (current == '(') {
-#ifdef DEBUG
-													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("(\n");
-#endif
-		++(*parenthesiscount);
-		number = calcline(0, '+', error, parenthesiscount);
+	int current = spaceskip(error);
+	if (*error)
+		return 0;
+
+	if (!is_number(current)) {
+		*error = 2;
+		return 0;
+	}
+
+	int number = 0;
+	while (is_number(current)) {
+		number *= 10;
+		number += current - '0';
+		current = read(error);
 		if (*error)
 			return 0;
-#ifdef DEBUG
-													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf(")\n");
-#endif
-	} else {
-		int sign = 1;
-		if (is_sign(current)) {
-			if (current == '-')
-				sign = -1;
-			current = spaceskip(error);
-			if (*error)
-				return 0;
+	}
+	ungetc(current, stdin);
+	return number;
+}
 
-			if (is_end(current)) {
-				ungetc(current, stdin);
-				*error = 3;
-				return 0;
-			}
-			else if (is_sign(current)) {
-				*error = 6;
-				return 0;
-			}
-		}
-		else if (!is_number(current)) {
-			*error = 2;
+int read_snumber(int *error, int *parenthesiscount)
+{
+	int current = spaceskip(error);
+	int sign = 1;
+	if (is_sign(current)) {
+		if (current == '-')
+			sign = -1;
+		current = spaceskip(error);
+		if (*error)
+			return 0;
+
+		if (is_end(current)) {
+			ungetc(current, stdin);
+			*error = 3;
 			return 0;
 		}
-		for (number = 0;is_number(current);current = read(error)) {
-			if (*error)
-				return 0;
-			number *= 10;
-			number += current - '0';
+		else if (is_sign(current)) {
+			*error = 6;
+			return 0;
 		}
-		number *= sign;
+	}
+	ungetc(current, stdin);
+	int number = read_unumber(error);
+	if (*error)
+		return 0;
+
+	number *= sign;
+
+#ifdef DEBUG
+	debug_tabulate(*parenthesiscount);
+	printf("number is: %d\n", number);
+#endif
+	return number;
+}
+
+int read_expression(int prevnumber, int operator, int *error, int *parenthesiscount);
+int read_factor(int *error, int *parenthesiscount)
+{
+	int number;
+	int current = spaceskip(error);
+	if (current == '(') {
+#ifdef DEBUG
+	debug_tabulate(*parenthesiscount);
+	printf("(\n");
+#endif
+		++(*parenthesiscount);
+		number = read_expression(0, '+', error, parenthesiscount);
+		if (*error)
+			return 0;
+
+		current = read(error);
+		if (*error)
+			return 0;
+
+		if (current == ')') {
+			--(*parenthesiscount);
+		} else {
+			ungetc(current, stdin);
+			*error = 7;
+			return 0;
+		}
+#ifdef DEBUG
+	debug_tabulate(*parenthesiscount);
+	printf(")\n");
+#endif
+	} else {
+		ungetc(current, stdin);
+		number = read_snumber(error, parenthesiscount);
+	}
+	return number;
+}
+
+int read_term(int *error, int *parenthesiscount)
+{
+	int number = read_factor(error, parenthesiscount);
+	if (*error)
+		return 0;
+
+	int current = spaceskip(error);
+	if (*error)
+		return 0;
+
+	if (current == '*') {
+		number *= read_term(error, parenthesiscount);
+		if (*error)
+			return 0;
+	} else {
 		ungetc(current, stdin);
 	}
 	return number;
 }
 //======================================================================================================================================================
-int calcline(int prevnumber, int operator, int *error, int *parenthesiscount)
+int read_expression(int prevnumber, int operator, int *error, int *parenthesiscount)
 {
 	int number, operatornew, current = spaceskip(error);
 	if (*error)
 		return 0;
 #ifdef DEBUG
-													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");} printf("-  -  -  -  -  -  -  -  -  \n");for (int i = 0;i<*parenthesiscount;i++) {printf("\t");} printf("initial prevnumber is: %d\noperator is: %c\n", prevnumber, operator);
+													debug_tabulate(*parenthesiscount);  printf("-  -  -  -  -  -  -  -  -  \n");
+													debug_tabulate(*parenthesiscount);  printf("initial prevnumber is: %d\n", prevnumber);
+													debug_tabulate(*parenthesiscount);  printf("operator is: %c\n", operator);
 #endif
 	if (is_end(current)) {
 		ungetc(current, stdin);
@@ -163,49 +252,34 @@ int calcline(int prevnumber, int operator, int *error, int *parenthesiscount)
 	}
 
 	while (!is_end(current)) {
-		number = get_number(current, error, parenthesiscount);
+		ungetc(current, stdin);
+		number = read_term(error, parenthesiscount);
 		if (*error)
 			return 0;
-#ifdef DEBUG
-													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("number is: %d\n", number);
-#endif
+
 		operatornew = get_operator(error);
 			if (*error)
 				return 0;
 #ifdef DEBUG
-													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("the operatornew is: %c\n", operatornew);
+													debug_tabulate(*parenthesiscount); printf("the operatornew is: %c\n", operatornew);
 #endif
-		if (operatornew == '*' && current != '\n') {
-			number = calcline(number, '*', error, parenthesiscount);
-			operatornew = get_operator(error);
-			if (*error)
-				return 0;
-#ifdef DEBUG
-													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("multnumber is: %d\nnewtoperator is: %c\n", number, operatornew);
-#endif
-		}
 		if (operator == '+') {
 			prevnumber += number;
 		}
 		else if (operator == '-') {
 			prevnumber -= number;
 		}
-		else if (operator == '*') {
-			if (operatornew)
-				ungetc(operatornew, stdin);
-			return (prevnumber * number);
-		}
 		operator = operatornew;
 #ifdef DEBUG
-													for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("-----------------------------\n");for (int i = 0;i<*parenthesiscount;i++) {printf("\t");}printf("prevnumber is: %d\nnewoperator is: %c\n", prevnumber, operator);
+													debug_tabulate(*parenthesiscount); printf("-----------------------------\n");
+													debug_tabulate(*parenthesiscount); printf("prevnumber is: %d\n", prevnumber);
+													debug_tabulate(*parenthesiscount); printf("newoperator is: %c\n", operator);
 #endif
 		current = spaceskip(error);
 		if (*error)
 			return 0;
 	}
-	if (current == ')')
-		--(*parenthesiscount);
-	if (current == '\n')
+	if (is_end(current))
 		ungetc(current, stdin);
 	if (operatornew != 0)
 		*error = 3;
@@ -219,19 +293,28 @@ int main()
 	for (int counter = 1; error != 5; counter++) {
 		error = 0;
 		int parenthesiscount = 0;
-		int result = calcline(0, '+', &error, &parenthesiscount);
-		printf("Result %d: ", counter);
-		if (error || parenthesiscount)
-			handle_error(&error, &parenthesiscount);
-		else
-			printf("%d\n", result);
-#ifdef DEBUG
-													printf("Remaining stdin:[%c", current);
-#endif
+		int result = read_expression(0, '+', &error, &parenthesiscount);
+
 		int current = 0;
+		if (error) {
+			handle_error(&error, &parenthesiscount);
+		} else {
+			current = read(&error);
+			if (current == ')') {
+				error = 8;
+				handle_error(&error, &parenthesiscount);
+			} else {
+				printf("Result %d: ", counter);
+				printf("%d\n", result);
+			}
+		}
+#ifdef DEBUG
+													printf("Remaining stdin:[");
+#endif
 		while (current != '\n' && error != 5) {
 			current = read(&error);
 #ifdef DEBUG
+												if (current != '\n')
 													printf("%c", current);
 #endif
 		}
